aed0ae29de6b68d808fd9620f4f9a22f84318bce
NitishKumarShrivastav <kumarshrivastav2001@gmail.com>
2025-08-07 23:30:47 +0530

    This is my new branch

--- File1.sh
+++ File1.sh
@@ -2,3 +2,4 @@ This is pull command practice
 We will do the push command
 We will pratice on commit changes
 We are doing practice of pull & push command
+We have created new branch in thi remot repo
